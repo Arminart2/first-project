300593e4e69a40e444ba14e395bd35508bbdc64f
vallieharmin8@gmail.com <vallieharmin8@gmail.com>
2024-02-24 17:52:36 +0330

    create-first-route

---
+++ app/games/page.tsx
@@ -0,0 +1,7 @@
+const Games = () => {
+    return <><div className="text-lg text-green-600">Game List</div>
+    </>
+  
+  }
+  
+  export default Games
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 const Home = () => {
-  return <div className="text-lg text-green-600">Armin-project</div>
+  return <><div className="text-lg text-green-600">
+    <ul>
+      <li>
+        <a href="/games">Armin-project</a>
+      </li>
+    </ul>
+  </div>
+  <div> art</div></>
 
 }
 
